669d984aa6b8d65d79db4070f2b431c497780c95
Codesmith <charlesbasoah93@gmail.com>
2022-02-08 21:54:50 +0000

    First footer ui design

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,60 @@
+<footer>
+  <div class="p-10 bg-slate-800 text-gray-300">
+    <div class="max-w-7xl mx-auto">
+      <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-2">
+        <div class="mb-5">
+          <h4 class="text-2xl pb-4 text-cyan-600">NikmaGus</h4>
+             <p class="text-gray-500"> Spintex road, Accra <br>
+                Opposite GCB Bank <br><br>
+                <strong>Phone:</strong>+2330540000000 <br>
+                <strong>Email:</strong>nikmagus@info.com
+              </p>
+        </div>
+        <div class="mb-5">
+          <h4 class="text-yellow-500">Useful Links</h4>
+          <ul class="text-gray-500">
+            <li class="pb-1"><i class="fa fa-chevron-right text-cyan-600"></i><a href="" class="hover:text-yellow-500"> Home</a></li>
+            <li class="pb-1"><i class="fa fa-chevron-right text-cyan-600"></i><a href="" class="hover:text-yellow-500"> About us</a></li>
+            <li class="pb-1"><i class="fa fa-chevron-right text-cyan-600"></i><a href="" class="hover:text-yellow-500"> Services</a></li>
+            <li class="pb-1"><i class="fa fa-chevron-right text-cyan-600"></i><a href="" class="hover:text-yellow-500"> Terms of services</a></li>
+          </ul>
+        </div>
+        <div class="mb-5">
+        <h4 class="text-yellow-500">Our services</h4>
+          <ul class="text-gray-500">
+            <li class="pb-1"><i class="fa fa-chevron-right text-cyan-600"></i><a href="" class="hover:text-yellow-500"> Motion graphics</a></li>
+            <li class="pb-1"><i class="fa fa-chevron-right text-cyan-600"></i><a href="" class="hover:text-yellow-500"> 2D & 3D animations</a></li>
+            <li class="pb-1"><i class="fa fa-chevron-right text-cyan-600"></i><a href="" class="hover:text-yellow-500"> 3D architecture & prototyping</a></li>
+            <li class="pb-1"><i class="fa fa-chevron-right text-cyan-600"></i><a href="" class="hover:text-yellow-500"> 2D architecture & prototyping</a></li>
+          </ul>
+        </div>
+        <div class="mb-5">
+          <h4 class="text-yellow-500 pb-4">Subscribe to our newsletter</h4>
+          <p class="text-gray-500 pb-2">Join over 500+ of people and never miss our updates on printing and more</p>
+          <form action="" method="post" class="flex flex-row flex-wrap">
+            <input type="text" name="" id="" class="text-gray-500 w-2/3 p-2 focus:border-cyan-600" placeholder="email@example.com">
+            <button class="p-2 w-1/3 bg-cyan-600 text-white hover:bg-cyan-600">Subscribe</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="w-full bg-cyan-700 text-gray-300 px-10">
+    <div>
+      <div class="text-center">
+        <div>
+          Copyright@<strong><span>NikmaGus</span></strong>. All Right Reserved
+        </div>
+        <!-- <div>
+          Designed by <a href="" class="text-yellow-500">ARC incorporation</a>
+        </div> -->
+        <div class="pt-2 pb-2">
+          <a href="" class="w-10 h-10 rounded-full bg-yellow-600 hove:bg-yellow-600 mx-1 inline-block pt-1"><i class="fab fa-facebook"></i></a>
+          <a href="" class="w-10 h-10 rounded-full bg-yellow-600 hove:bg-yellow-500 mx-1 inline-block pt-1"><i class="fab fa-instagram"></i></a>
+          <a href="" class="w-10 h-10 rounded-full bg-yellow-600 hove:bg-yellow-500 mx-1 inline-block pt-1"><i class="fab fa-twitter"></i></a>
+          <a href="" class="w-10 h-10 rounded-full bg-yellow-600 hove:bg-yellow-500 mx-1 inline-block pt-1"><i class="fab fa-linkedin"></i></a>
+        </div>
+      </div>
+    </div>
+  </div>
+</footer>
